267f83e2457c81f06b319192ebc00d2c7b10f7bd
contygm <contygm@gmail.com>
2019-05-21 01:49:28 -0700

    proj: attendance page table populates with json, so does email page

--- project/server.js
+++ project/server.js
@@ -5,7 +5,18 @@ var referrals = require('./json/referrals');
 
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+//TODO: cite
+//https://stackoverflow.com/questions/32707322/how-to-make-a-handlebars-helper-global-in-expressjs
+var handlebars = require('express-handlebars').create({
+  defaultLayout:'main',
+  helpers: { // adding helper
+    inc: function(value, options) {
+      return parseInt(value) + 1;
+    }
+  }
+});
+
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -22,19 +33,27 @@ app.get('/',function(req,res){
 });
 
 app.get('/attendance',function(req,res){
-  console.log("MONITORS", monitors);
-  console.log("REFERRALS", referrals);
+  var context = {};
+  context.all_referrals = referrals.all_referrals;
 
-  res.render('attendance');
+  res.render('attendance', context);
 });
 
 app.get('/email', function(req,res){
-  res.render('email');
+  var context = {};
+  context.all_monitors = monitors.all_monitors;
+  context.all_referrals = referrals.all_referrals;
+  res.render('email', context);
 });
 
 app.get('/referral', function(req,res){
   res.render('referral');
 });
+
+// app.post('/referral', function(req,res){
+  //referrals.all_referrals[0].student.name = {"first":"Sally", "last": "Espinoza"};
+//   res.render('refer-submit');
+// });
 
 app.get('/referral/submit', function(req,res){
   res.render('refer-submit');
