7aea5b48563a66cf6f6ec8f510dde9899b1c57f8
contygm <contygm@gmail.com>
2019-05-26 14:20:49 -0700

    proj: no validity for referral

--- project/public/js/referral.js
+++ project/public/js/referral.js
@@ -1,40 +1,35 @@
 function submitRef() {
-
-    // .submit() allow for form validation
-    $('.referralForm').submit(function(event) {
-        event.preventDefault(); // circumvents the default submit function
-        const newReferral = {
-            _id: 999,
-            date : `${$('#refer-date').val()}`,
-            reason : `${$('#refer-reason').val()}`,
-            description: `${$('#refer-other').val()}`,
-            time_served: {
-                status: "NOT_SERVED",
-                date : `${$('#serve-date').val()}`
+    const newReferral = {
+        _id: 999,
+        date : `${$('#refer-date').val()}`,
+        reason : `${$('#refer-reason').val()}`,
+        description: `${$('#refer-other').val()}`,
+        time_served: {
+            status: "NOT_SERVED",
+            date : `${$('#serve-date').val()}`
+        },
+        teacher: {
+            name: {
+                first: `${$('#teach-firstName').val()}`,
+                last: `${$('#teach-lastName').val()}`
             },
-            teacher: {
-                name: {
-                    first: `${$('#teach-firstName').val()}`,
-                    last: `${$('#teach-lastName').val()}`
-                },
-                email: `${$('#teach-email').val()}`
+            email: `${$('#teach-email').val()}`
+        },
+        student: {
+            name: {
+                first: `${$('#stud-firstName').val()}`,
+                last: `${$('#stud-lastName').val()}`
             },
-            student: {
-                name: {
-                    first: `${$('#stud-firstName').val()}`,
-                    last: `${$('#stud-lastName').val()}`
-                },
-                grade: `${$('#stud-grade').val()}`
-            }
+            grade: `${$('#stud-grade').val()}`
         }
-    
-        var http = new XMLHttpRequest();
-        http.open('POST', `/newReferral`, true);
-        http.setRequestHeader('Content-Type', 'application/json');
-        http.addEventListener('load', function(){
-            const res = JSON.parse(http.response);
-            window.location = res.redirectUrl;
-        });
-        http.send(JSON.stringify(newReferral));
-    })
+    }
+
+    var http = new XMLHttpRequest();
+    http.open('POST', `/newReferral`, true);
+    http.setRequestHeader('Content-Type', 'application/json');
+    http.addEventListener('load', function(){
+        const res = JSON.parse(http.response);
+        window.location = res.redirectUrl;
+    });
+    http.send(JSON.stringify(newReferral));
 }
