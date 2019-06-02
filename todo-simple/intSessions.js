e91f2898aecae042d709e8f2d0c74231145de5c5
contygm <contygm@gmail.com>
2019-06-01 22:45:12 -0700

    hw5: connected to db locally, basic form up, table handle bars set up

---
+++ todo-simple/intSessions.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var request = require('request')
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({secret:'SuperSecretPassword'}));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res,next){
+  var context = {};
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length || 0;
+  context.toDo = req.session.toDo || [];
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
+app.post('/',function(req,res){
+  var context = {};
+
+  if(req.body['New List']){
+    req.session.name = req.body.name;
+    req.session.toDo = [];
+    req.session.curId = 0;
+  }
+
+  //If there is no session, go to the main page.
+  if(!req.session.name){
+    res.render('newSession', context);
+    return;
+  }
+
+  if(req.body['Add Item']){
+    console.log(req.body)
+    req.session.toDo.push({"name":req.body.name, "id":req.session.curId, "city":req.body.city, "maxTemp":req.body.maxTemp});
+
+    request(`http://api.openweathermap.org/data/2.5/weather?q=${req.body.city}&units=imperial&APPID=fa7d80c48643dfadde2cced1b1be6ca1`, function(err, response, body){
+      if(!err && response.statusCode < 400){
+        const json = JSON.parse(body);
+        console.log(json.main.temp_max);
+
+      } else {
+        if(response){
+          console.log(response.statusCode);
+        }
+      }
+    }, );
+    req.session.curId++;
+  }
+
+  if(req.body['Done']){
+    req.session.toDo = req.session.toDo.filter(function(e){
+      return e.id != req.body.id;
+    })
+  }
+
+  context.name = req.session.name;
+  context.toDoCount = req.session.toDo.length;
+  context.toDo = req.session.toDo;
+  // context.city = req.session.city;
+  // context.maxTemp = req.session.maxTemp;
+  console.log(context.toDo);
+  res.render('toDo',context);
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
